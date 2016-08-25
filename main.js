9d4237997166fd2a9c9c9036232f4d6633b65eb6
Ritika <ritikamittal1701@gmail.com>
2016-08-25 22:26:34 +0530

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 
 function play(){
-	var r=parseInt(document.getElementById('angle').value);
-    
+	var ang=parseInt(document.getElementById('angle').value);
+	var vel=parseInt(document.getElementById('velocity').value);
+	var weap=parseInt(Math.random()*8+1);
+	var ele=document.getElementById('weapon');
+	switch(weap){
+		case 1: ele.innerHTML="Single Shot";
+		break;
+		case 2: ele.innerHTML="Machine Gun";
+		break;
+		case 3: ele.innerHTML="Blow Torch";
+		break;
+		case 4: ele.innerHTML="Acid Rain";
+		break;
+		case 5: ele.innerHTML="Magic Wall";
+		break;
+		case 6: ele.innerHTML="Time Bomb";
+		break;
+		case 7: ele.innerHTML="Snipper";
+		break;
+		case 8: ele.innerHTML="Laser";
+	    break;
+	}    
 }
